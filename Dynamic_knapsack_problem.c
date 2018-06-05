79bae036ec495aeb50482cfc42fb7273555394ea
Claireeeee <31379115+Claireeeee@users.noreply.github.com>
2018-06-05 09:34:57 +0800

    gave up recursion. build array from base

--- Dynamic_knapsack_problem.c
+++ Dynamic_knapsack_problem.c
@@ -10,32 +10,32 @@
 
 //背包容积，宝石体积和价值，找价值最大化的装配方法
 
-//可改进之处：1，可不可以在一开始加一个筛选，比如这里直接把体积大且平均价值小的宝石筛掉
-//         2,可不可以把中间结果放数组里。重复计算过程的结果可以储存?       如果体积大小重合的多，重复计算过程就多，可以加一个判断,决定是否存储中间结果     3，这里的宝石不能重复拿
+//规模线性减小的递归复杂度是指数级的，不能用。之前的最佳期望就是。可以通过循环存储数据后调用，避免递归。从底部开始搭建要用的数据数组，而不是直接从顶部调用
+
+//用ij循环构建中间数据。因为每次需要的是小一层的i下不同的j下的最大值，所以j一定要遍历，而且原则上还要用i在二维数组中标记。实际使用中，需要的只是之前i下j下的最大值，可以考虑随着i的生长更新j下值，而不用i标记。从j的角度看，不同的i下，j是要往前调用的，如果从前往后更新，会在往前调用的时候出错，所以要从后往前更新
+
 
 
 #include <stdio.h>
 #include <stdlib.h>
+#define NUMBER 5
+#define VOLUME 10
 
 
-typedef int (*dataarray)[2] ;                                         //二维数组存储数据
-int totalvalue(dataarray data,int nowjewel,int leftroom,int number);   //递归函数
+
 
 int main(void){
     int data[5][2]={2,6,2,3,6,5,5,4,4,6};
-    printf("%d\n",totalvalue(data,0,10,5));
+    int maxvalue_under_j[VOLUME]={0,0,0,0,0,0,0,0,0,0};
+    for(int i=0;i<NUMBER;i++){                                 //i放在这只是为了标示当前判断的宝石，参与存储的数组只以j为索引（标示）
+        for(int j=VOLUME-1;j>=data[i][0];j--){                  //j从最大值开始遍历
+            int a=maxvalue_under_j[j];
+            int b=maxvalue_under_j[j-data[i][0]]+data[i][1];
+            //printf("i: %d  :%d",a,b);
+            maxvalue_under_j[j]=(a>b)?a:b;
+        }
+        
+    }
+    printf("%d\n",maxvalue_under_j[9]);
 }
 
-int totalvalue(dataarray data,int nowjewel,int leftroom,int number){
-    int tovalue=0;
-    if (leftroom-data[nowjewel][0]<0||nowjewel>=number) {
-        return 0;
-    }
-    else{
-        int a=totalvalue(data, nowjewel+1,leftroom,number);
-        int b=totalvalue(data,nowjewel+1,leftroom-data[nowjewel][0],number)+data[nowjewel][1];
-        tovalue=(a>b)?a:b;
-        //printf("nowjewel:%d   a:%d   b:%d\n",nowjewel,a,b);;
-        return tovalue;
-    }
-}
